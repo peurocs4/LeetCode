65900d5eafd6aa03fae02f7f9a736c72d067cb4f
CSKim <peurocs4@gmail.com>
2020-09-28 19:29:51 +0900

    Refactored + improved performance

--- LeetCode/MinimumCostToHireKWorkers.cpp
+++ LeetCode/MinimumCostToHireKWorkers.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
-#include <set>
+#include <queue>
 
 using namespace std;
 
@@ -14,11 +14,10 @@ class Solution {
 public:
   struct Worker {
     int quality;
-    int wage;
     double rate;
 
-    Worker(int quality, int wage, double rate) 
-      :quality(quality), wage(wage), rate(rate) {}
+    Worker(int quality, double rate) 
+      :quality(quality), rate(rate) {}
   };
 
   struct RateComparator {
@@ -27,44 +26,34 @@ public:
     }
   };
 
-  struct QualityComparator {
-    bool operator()(const Worker& worker1, const Worker& worker2) const {
-      return worker1.quality <= worker2.quality;
-    }
-  };
-
   double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int K) {
     vector<Worker> workers;
-
     for (int i = 0; i < quality.size(); ++i) {
       const auto& q = quality[i];
       const auto& w = wage[i];
       const auto r = static_cast<double>(w) / q;
-      workers.push_back(*new Worker(q, w, r));
+      workers.push_back(*new Worker(q, r));
     }
 
     sort(workers.begin(), workers.end(), RateComparator());
 
-    set<Worker, QualityComparator> s;
-    double minCost = static_cast<double>(INT_MAX);
-    for (int i = 0; i < workers.size(); ++i) {
-      const auto& worker = workers[i];
+    double minCost = DBL_MAX;
+    priority_queue<int> pq;
+    int accumQuality = 0;
+    for (const auto& worker : workers) {
+      accumQuality += worker.quality;
+      pq.push(worker.quality);
 
-      if (i >= K - 1) {
-        int accumQuality = worker.quality;
-        auto it = s.begin();
-        for (int j = 1; j < K; ++j) {
-          accumQuality += it->quality;
-          ++it;
-        }
-
-        double cost = accumQuality * worker.rate;
+      if (pq.size() > K) {
+        accumQuality -= pq.top();
+        pq.pop();
+      }
+      if (pq.size() == K) {
+        const double cost = accumQuality * worker.rate;
         minCost = min(minCost, cost);
       }
-
-      s.insert(worker);
     }
-
+    
     return minCost;
   }
 };
